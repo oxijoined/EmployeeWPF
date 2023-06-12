e6c189790c863e70e5ab66fb5a0b4b44044d6779
okcu <dev@okcuu.ru>
2023-06-12 18:35:56 +0300

    Refactoring and minor readability improvements

--- Employee/MainWindow.xaml.cs
+++ Employee/MainWindow.xaml.cs
@@ -24,79 +24,112 @@ namespace Employee
             InitializeComponent();
         }
 
+        // Обработчик события загрузки окна
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             ReadJsonAndAppendDataGrid("export.json");
         }
 
+        // Запрет редактирования ячеек в DataGrid
         private void employeesGrid_BeginningEdit(object sender, DataGridBeginningEditEventArgs e)
         {
             e.Cancel = true;
         }
 
+        // Обработчик нажатия кнопки "Добавить"
         private void AddNew_Click(object sender, RoutedEventArgs e)
         {
+            // Получение списка сотрудников из DataGrid
             List<EmployeeClass> employees = employeesGrid.Items.OfType<EmployeeClass>().ToList();
-            int newId = employees.Max(emp => emp.ID) + 1;
+
+            // Генерация нового идентификатора
+            int newId = employees.Count + 1;
+
+            // Создание нового экземпляра класса EmployeeClass
             EmployeeClass newEmployee = new EmployeeClass(newId, "", "", "", "", "", "", "", "");
 
+            // Открытие окна редактирования с передачей нового сотрудника и списка всех сотрудников
             EditEmployeeWindow editWindow = new EditEmployeeWindow(newEmployee, employees);
             editWindow.ShowDialog();
 
+            // Добавление нового сотрудника в список и обновление DataGrid
             employees.Add(newEmployee);
             employeesGrid.ItemsSource = employees;
             employeesGrid.Items.Refresh();
         }
 
+        // Обработчик нажатия кнопки "Редактировать"
         private void EditRecordButton_Click(object sender, RoutedEventArgs e)
         {
+            // Получение выбранного сотрудника из DataContext кнопки
             Button button = (Button)sender;
             EmployeeClass employee = (EmployeeClass)button.DataContext;
 
+            // Получение списка сотрудников из DataGrid
             List<EmployeeClass> employees = employeesGrid.Items.OfType<EmployeeClass>().ToList();
 
+            // Открытие окна редактирования с передачей выбранного сотрудника и списка всех сотрудников
             EditEmployeeWindow editWindow = new EditEmployeeWindow(employee, employees);
             editWindow.ShowDialog();
+
+            // Обновление DataGrid
             employeesGrid.Items.Refresh();
         }
 
+        // Обработчик нажатия кнопки "Удалить"
         private void DeleteRecordButton_Click(object sender, RoutedEventArgs e)
         {
+            // Получение выбранного сотрудника из DataContext кнопки
             Button button = (Button)sender;
             EmployeeClass employee = (EmployeeClass)button.DataContext;
 
+            // Получение списка сотрудников из DataGrid
             if (employeesGrid.ItemsSource is List<EmployeeClass> employees)
             {
+                // Удаление выбранного сотрудника из списка и обновление DataGrid
                 employees.Remove(employee);
                 employeesGrid.Items.Refresh();
             }
         }
 
+        // Обработчик нажатия кнопки "Импорт"
         private void ImportButton_Click(object sender, RoutedEventArgs e)
         {
+            // Открытие диалогового окна для выбора файла
             OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.Filter = "JSON Files (*.json)|*.json";
 
             if (openFileDialog.ShowDialog() == true)
             {
+                // Получение пути выбранного JSON-файла и чтение данных из него
                 string jsonFilePath = openFileDialog.FileName;
                 ReadJsonAndAppendDataGrid(jsonFilePath);
             }
         }
 
+        // Обработчик нажатия кнопки "Экспорт и сохранение"
         private void ExportSaveButton_Click(object sender, RoutedEventArgs e)
         {
+            // Получение списка сотрудников из DataGrid
             List<EmployeeClass> employees = employeesGrid.Items.OfType<EmployeeClass>().ToList();
+
+            // Сериализация списка сотрудников в формате JSON
             string json = JsonConvert.SerializeObject(employees, Formatting.Indented);
+
+            // Сохранение JSON-данных в файл "export.json"
             File.WriteAllText("export.json", json);
+
+            // Вывод сообщения об успешном сохранении
             MessageBox.Show($"Успешно сохранено {employees.Count} записей");
         }
 
+        // Обработчик нажатия кнопки "Сбросить фильтр"
         private void CancelFilter_Click(object sender, RoutedEventArgs e)
         {
-            foreach (EmployeeClass employee in employeesGrid.Items)
+            // Сброс фильтрации и восстановление видимости всех строк в DataGrid
+            for (int i = 0; i < employeesGrid.Items.Count; i++)
             {
-                DataGridRow row = (DataGridRow)employeesGrid.ItemContainerGenerator.ContainerFromItem(employee);
+                DataGridRow row = employeesGrid.ItemContainerGenerator.ContainerFromIndex(i) as DataGridRow;
 
                 if (row != null)
                 {
@@ -107,26 +140,32 @@ namespace Employee
                             var cellContent = column.GetCellContent(row);
                             if (cellContent is TextBlock textBlock)
                             {
+                                // Сброс цвета фона ячейки
                                 textBlock.Background = Brushes.White;
-                                row.Visibility = Visibility.Visible;
                             }
                         }
                     }
+
+                    // Восстановление видимости строки
+                    row.Visibility = Visibility.Visible;
                 }
             }
         }
 
+        // Обработчик нажатия кнопки "Поиск"
         private void SearchSelection_Click(object sender, RoutedEventArgs e)
         {
+            // Проверка наличия фильтра
             if (FilterInput.Text == "")
             {
                 MessageBox.Show("Необходимо ввести фильтр");
                 return;
             }
 
-            foreach (EmployeeClass employee in employeesGrid.Items)
+            // Поиск и подсветка ячеек, содержащих текст фильтра
+            for (int i = 0; i < employeesGrid.Items.Count; i++)
             {
-                DataGridRow row = (DataGridRow)employeesGrid.ItemContainerGenerator.ContainerFromItem(employee);
+                DataGridRow? row = employeesGrid.ItemContainerGenerator.ContainerFromIndex(i) as DataGridRow;
 
                 if (row != null)
                 {
@@ -140,10 +179,12 @@ namespace Employee
                                 string cellText = textBlock.Text;
                                 if (cellText.Contains(FilterInput.Text))
                                 {
+                                    // Подсветка ячейки совпадающего текста
                                     textBlock.Background = Brushes.Yellow;
                                 }
                                 else
                                 {
+                                    // Восстановление цвета фона ячейки
                                     textBlock.Background = Brushes.White;
                                 }
                             }
@@ -153,44 +194,18 @@ namespace Employee
             }
         }
 
-
-        private void FilterButton_Click(object sender, RoutedEventArgs e)
-        {
-            foreach (EmployeeClass employee in employeesGrid.Items)
-            {
-                DataGridRow row = (DataGridRow)employeesGrid.ItemContainerGenerator.ContainerFromItem(employee);
-
-                if (row != null)
-                {
-                    foreach (DataGridColumn column in employeesGrid.Columns)
-                    {
-                        if (column is DataGridTextColumn textColumn)
-                        {
-                            var cellContent = column.GetCellContent(row);
-                            if (cellContent is TextBlock textBlock)
-                            {
-                                string cellText = textBlock.Text;
-                                if (cellText.Contains(FilterInput.Text))
-                                {
-                                    row.Visibility = Visibility.Visible;
-                                }
-                                else
-                                {
-                                    row.Visibility = Visibility.Collapsed;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
+        // Чтение данных из JSON-файла и добавление их в DataGrid
         private void ReadJsonAndAppendDataGrid(string filePath)
         {
             if (File.Exists(filePath))
             {
+                // Чтение JSON-данных из файла
                 string json = File.ReadAllText(filePath);
+
+                // Десериализация JSON-данных в список сотрудников
                 List<EmployeeClass> employees = JsonConvert.DeserializeObject<List<EmployeeClass>>(json);
+
+                // Установка списка сотрудников в источник данных для DataGrid
                 employeesGrid.ItemsSource = employees;
             }
             else
